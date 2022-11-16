6d8d457381b152c57a494c92597aab34e13c21de
Bilal Yousef <s1169284@student.windesheim.nl>
2022-11-16 13:33:41 +0100

    commit

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,10 +1,58 @@
 const mariadb = require("mariadb");
+
 const pool = mariadb.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "Ventosport",
-  connectionLimit: 13,
+  host: "109.237.211.172",
+  user: "test",
+  password: "Welkom",
+  connectionLimit: 5,
+  database: "Ventosportdb",
 });
+
+
+pool
+  .getConnection()
+  .then((conn) => {
+    conn
+      .query("SELECT 1 as val")
+      .then((rows) => {
+        console.log(rows); //[ {val: 1}, meta: ... ]
+
+        return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+      })
+      .then((res) => {
+        console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+        conn.end();
+      })
+      .catch((err) => {
+        //handle error
+        console.log(err);
+        conn.end();
+      });
+  })
+  .catch((err) => {
+    //not connected
+  });
+
+async function asyncFunction() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT 1 as val");
+    console.log(rows); //[ {val: 1}, meta: ... ]
+    const res = await conn.query("INSERT INTO myTable value (?, ?)", [
+      1,
+      "mariadb",
+    ]);
+    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) return conn.end();
+  }
+}
+
+function asyncFunction();
+
 async function asyncFunction() {
   let conn;
   try {
