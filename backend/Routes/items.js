8715dc4a357ef48f2c8fafb855db6173709f4a65
DDuijker <djoekeduijker@live.nl>
2022-12-02 00:04:07 +0100

    changed names from item to listing

--- backend/Routes/items.js
+++ backend/Routes/items.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../database/connection");
 
-//get all items
-router.get("/items", async function (req, res) {
+//get all listings
+router.get("/listings", async function (req, res) {
   pool
     .getConnection()
     .then((conn) => {
-      conn.query("SELECT * from items").then((rows) => {
+      conn.query("SELECT * from listing").then((rows) => {
         res.json(rows);
         conn.end();
       });
@@ -15,42 +15,40 @@ router.get("/items", async function (req, res) {
     .catch((err) => res.status(400).json("Error " + err));
 });
 
-//get items sorted by date
-router.get("/items/recent", async function (req, res) {
+//get listings sorted by date
+router.get("/listings/recent", async function (req, res) {
   pool
     .getConnection()
     .then((conn) => {
-      conn
-        .query("SELECT * from items ORDER BY date_added DESC")
-        .then((rows) => {
-          res.json(rows);
-          conn.end();
-        });
+      conn.query("SELECT * from listing ORDER BY date DESC").then((rows) => {
+        res.json(rows);
+        conn.end();
+      });
     })
     .catch((err) => res.status(400).json("Error " + err));
 });
 
-//get items sorted by popularity
-router.get("/items/popular", async function (req, res) {
-  pool
-    .getConnection()
-    .then((conn) => {
-      conn
-        .query("SELECT * from items ORDER BY popularity DESC")
-        .then((rows) => {
-          res.json(rows);
-          conn.end();
-        });
-    })
-    .catch((err) => res.status(400).json("Error " + err));
-});
+// //get listings sorted by popularity
+// router.get("/items/popular", async function (req, res) {
+//   pool
+//     .getConnection()
+//     .then((conn) => {
+//       conn
+//         .query("SELECT * from items ORDER BY popularity DESC")
+//         .then((rows) => {
+//           res.json(rows);
+//           conn.end();
+//         });
+//     })
+//     .catch((err) => res.status(400).json("Error " + err));
+// });
 
 //get items sorted by price (low to high)
-router.get("/items/price-low", async function (req, res) {
+router.get("/listings/price-low", async function (req, res) {
   pool
     .getConnection()
     .then((conn) => {
-      conn.query("SELECT * from items ORDER BY price ASC").then((rows) => {
+      conn.query("SELECT * from listing ORDER BY price ASC").then((rows) => {
         res.json(rows);
         conn.end();
       });
@@ -59,11 +57,11 @@ router.get("/items/price-low", async function (req, res) {
 });
 
 //get items sorted by price (high to low)
-router.get("/items/price", async function (req, res) {
+router.get("/listings/price", async function (req, res) {
   pool
     .getConnection()
     .then((conn) => {
-      conn.query("SELECT * from items ORDER BY price DESC").then((rows) => {
+      conn.query("SELECT * from listing ORDER BY price DESC").then((rows) => {
         res.json(rows);
         conn.end();
       });
