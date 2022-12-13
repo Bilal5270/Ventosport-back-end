201604b7c09f98ec26cfa4a1900b15704cbce48a
DDuijker <djoekeduijker@live.nl>
2022-12-13 22:43:56 +0100

    Listings actually responsive

--- backend/Routes/listings.js
+++ backend/Routes/listings.js
@@ -9,7 +9,7 @@ router.get("/", async function (req, res) {
     .then((conn) => {
       conn
         .query(
-          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing INNER JOIN categories ON listing.category = categories.category_id INNER JOIN subcategory ON subcategory.subcategory_id = listing.subcategory INNER JOIN users ON users.user_id = listing.user_id"
+          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing LEFT JOIN categories ON listing.category = categories.category_id LEFT JOIN subcategory ON subcategory.subcategory_id = listing.subcategory LEFT JOIN users ON users.user_id = listing.user_id"
         )
         .then((rows) => {
           res.json(rows);
@@ -26,7 +26,7 @@ router.get("/all", async function (req, res) {
     .then((conn) => {
       conn
         .query(
-          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing INNER JOIN categories ON listing.category = categories.category_id INNER JOIN subcategory ON subcategory.subcategory_id = listing.subcategory INNER JOIN users ON users.user_id = listing.user_id"
+          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing LEFT JOIN categories ON listing.category = categories.category_id LEFT JOIN subcategory ON subcategory.subcategory_id = listing.subcategory LEFT JOIN users ON users.user_id = listing.user_id"
         )
         .then((rows) => {
           res.json(rows);
@@ -43,7 +43,7 @@ router.get("/recent", async function (req, res) {
     .then((conn) => {
       conn
         .query(
-          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing INNER JOIN categories ON listing.category = categories.category_id INNER JOIN subcategory ON subcategory.subcategory_id = listing.subcategory INNER JOIN users ON users.user_id = listing.user_id ORDER BY listing.date DESC"
+          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing LEFT JOIN categories ON listing.category = categories.category_id LEFT JOIN subcategory ON subcategory.subcategory_id = listing.subcategory LEFT JOIN users ON users.user_id = listing.user_id ORDER BY listing.date DESC"
         )
         .then((rows) => {
           res.json(rows);
@@ -60,7 +60,7 @@ router.get("/low-to-high", async function (req, res) {
     .then((conn) => {
       conn
         .query(
-          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing INNER JOIN categories ON listing.category = categories.category_id INNER JOIN subcategory ON subcategory.subcategory_id = listing.subcategory INNER JOIN users ON users.user_id = listing.user_id ORDER BY price ASC"
+          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing LEFT JOIN categories ON listing.category = categories.category_id LEFT JOIN subcategory ON subcategory.subcategory_id = listing.subcategory LEFT JOIN users ON users.user_id = listing.user_id ORDER BY price ASC"
         )
         .then((rows) => {
           res.json(rows);
@@ -77,7 +77,7 @@ router.get("/high-to-low", async function (req, res) {
     .then((conn) => {
       conn
         .query(
-          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing INNER JOIN categories ON listing.category = categories.category_id INNER JOIN subcategory ON subcategory.subcategory_id = listing.subcategory INNER JOIN users ON users.user_id = listing.user_id ORDER BY price DESC"
+          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing LEFT JOIN categories ON listing.category = categories.category_id LEFT JOIN subcategory ON subcategory.subcategory_id = listing.subcategory LEFT JOIN users ON users.user_id = listing.user_id ORDER BY price DESC"
         )
         .then((rows) => {
           res.json(rows);
