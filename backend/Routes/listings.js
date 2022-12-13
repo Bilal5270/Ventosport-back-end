d94c3ff890b23080e67239bc1116890700e04b63
DDuijker <djoekeduijker@live.nl>
2022-12-13 19:10:24 +0100

    added price sorts to listings

--- backend/Routes/listings.js
+++ backend/Routes/listings.js
@@ -53,45 +53,38 @@ router.get("/recent", async function (req, res) {
     .catch((err) => res.status(400).json("Error " + err));
 });
 
-// //get listings sorted by popularity
-// router.get("/items/popular", async function (req, res) {
-//   pool
-//     .getConnection()
-//     .then((conn) => {
-//       conn
-//         .query("SELECT * from items ORDER BY popularity DESC")
-//         .then((rows) => {
-//           res.json(rows);
-//           conn.end();
-//         });
-//     })
-//     .catch((err) => res.status(400).json("Error " + err));
-// });
-
 //get items sorted by price (low to high)
-// router.get("/price-low-high", async function (req, res) {
-//   pool
-//     .getConnection()
-//     .then((conn) => {
-//       conn.query("SELECT * from listing ORDER BY price ASC").then((rows) => {
-//         res.json(rows);
-//         conn.end();
-//       });
-//     })
-//     .catch((err) => res.status(400).json("Error " + err));
-// });
+router.get("/low-to-high", async function (req, res) {
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query(
+          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing INNER JOIN categories ON listing.category = categories.category_id INNER JOIN subcategory ON subcategory.subcategory_id = listing.subcategory INNER JOIN users ON users.user_id = listing.user_id ORDER BY price ASC"
+        )
+        .then((rows) => {
+          res.json(rows);
+          conn.end();
+        });
+    })
+    .catch((err) => res.status(400).json("Error " + err));
+});
 
 // //get items sorted by price (high to low)
-// router.get("/price-high-low", async function (req, res) {
-//   pool
-//     .getConnection()
-//     .then((conn) => {
-//       conn.query("SELECT * from listing ORDER BY price DESC").then((rows) => {
-//         res.json(rows);
-//         conn.end();
-//       });
-//     })
-//     .catch((err) => res.status(400).json("Error " + err));
-// });
+router.get("/high-to-low", async function (req, res) {
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query(
+          "SELECT listing.*, users.city, categories.name AS category_name, subcategory.name AS subcategory_name FROM listing INNER JOIN categories ON listing.category = categories.category_id INNER JOIN subcategory ON subcategory.subcategory_id = listing.subcategory INNER JOIN users ON users.user_id = listing.user_id ORDER BY price DESC"
+        )
+        .then((rows) => {
+          res.json(rows);
+          conn.end();
+        });
+    })
+    .catch((err) => res.status(400).json("Error " + err));
+});
 
 module.exports = router;
