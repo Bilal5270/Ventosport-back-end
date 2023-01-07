9dac929b745fc99bb141d174d06563f60984016c
DDuijker <djoekeduijker@live.nl>
2023-01-07 14:50:29 +0100

    added liked query's for favorite

--- backend/Routes/listings.js
+++ backend/Routes/listings.js
@@ -62,21 +62,6 @@ router.get("/cities", async function (req, res) {
   });
 });
 
-// //get listings sorted by popularity
-// router.get("/items/popular", async function (req, res) {
-//   pool
-//     .getConnection()
-//     .then((conn) => {
-//       conn
-//         .query("SELECT * from items ORDER BY popularity DESC")
-//         .then((rows) => {
-//           res.json(rows);
-//           conn.end();
-//         });
-//     })
-//     .catch((err) => res.status(400).json("Error " + err));
-// });
-
 //get items sorted by price (low to high)
 router.get("/low-to-high", async function (req, res) {
   pool
@@ -110,5 +95,81 @@ router.get("/high-to-low", async function (req, res) {
     })
     .catch((err) => res.status(400).json("Error " + err));
 });
+
+router.get("/favorites/user/:user_id", async function (req, res) {
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query(
+          "SELECT * FROM liked WHERE liked.user_id = ?",
+          req.params.user_id
+        )
+        .then((rows) => {
+          res.json(rows);
+          conn.end();
+        });
+    })
+    .catch((err) => res.status(400).json("Error " + err));
+});
+
+//get a specific liked listing
+router.get("/favorites/:user_id/:listing_id", async function (req, res) {
+  const { user_id, listing_id } = req.params;
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query(
+          "SELECT * FROM liked WHERE user_id = ? AND listing_id = ?",
+          [user_id, listing_id],
+          (err, rows) => {
+            if (err) {
+              res.status(400).json("Error " + err);
+            } else {
+              res.json(rows);
+            }
+          }
+        )
+        .then((rows) => {
+          res.json(rows);
+          conn.end();
+        });
+    })
+    .catch((err) => res.status(400).json("Error " + err));
+});
+
+// add a favorite listing to an user
+router.post("/favorites/add", async function (req, res) {
+  const { user_id, listing_id } = req.body;
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query(
+          `INSERT INTO liked (user_id, listing_id) VALUES (${user_id}, ${listing_id})`
+        )
+        .then(conn.end());
+    })
+    .catch((err) => res.status(400).json("Error " + err));
+});
+
+//delete a liked listing
+router.delete(
+  "/favorites/delete/:user_id/:listing_id",
+  async function (req, res) {
+    const user_id = req.params.user_id;
+    const listing_id = req.params.listing_id;
+    const query = `DELETE FROM liked WHERE user_id = ${user_id} AND listing_id = ${listing_id}`;
+    pool
+      .getConnection()
+      .then((conn) => {
+        conn.query(query, [user_id, listing_id]).then((rows) => {
+          conn.end();
+        });
+      })
+      .catch((err) => res.status(400).json("Error " + err));
+  }
+);
 
 module.exports = router;
