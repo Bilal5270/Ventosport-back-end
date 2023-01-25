bca275b5a003130a8c12713272710bebaf803765
Bilal Yousef <s1169284@student.windesheim.nl>
2023-01-25 15:57:10 +0100

    commit afbeeldingen page

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ const cors = require("cors");
 var http = require("http");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require("path");
+const fs = require("fs");
+
+// app.use(express.static("uploads"));
 
 //Cors-anywhere
 app.use(cors());
@@ -13,11 +17,17 @@ app.use(express.json());
 const testRoute = require("./Routes/users");
 app.use("/users", testRoute);
 
-const adRoute = require('./Routes/PlaceAd');
-app.use('/placeAd', adRoute);
+const adRoute = require("./Routes/PlaceAd");
+app.use("/placeAd", adRoute);
 
 const listingRoute = require("./Routes/listings");
 app.use("/listings", listingRoute);
+
+app.get("/image/:fileName", function (req, res) {
+  const filePath = path.join(__dirname, "uploads", req.params.fileName);
+  res.type("image/png");
+  fs.createReadStream(filePath).pipe(res);
+});
 
 //Running server on Port
 app.listen(PORT, () => {
